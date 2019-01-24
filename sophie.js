71fdb547b9d17009f153a00a6d5b97699753788c
The Mastr <40341589+SonicMastr@users.noreply.github.com>
2019-01-23 23:02:56 -0600

    Update sophie.js

--- sophie.js
+++ sophie.js
@@ -133,7 +133,10 @@ client.on('message', message => {
 
 		if(has_admin == false) return message.reply('You don\'t have permission to use this command').then(msg => {msg.delete(5000);});
 
-		const total = args + 1;
+		const number2 = args.join(' ');
+		const x = parseInt(number2);
+		const y = 1;
+		const total = x + y;
 		const number = total;
 
 		message.channel.fetchMessages({ limit: number }).then(messages => message.channel.bulkDelete(messages));
